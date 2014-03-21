2bef14be38d41b570ad604505c59cb7a444934c7
rupert <rupert@2rmobile.com>
2014-03-22 08:08:41 +1100

    First Commit. Created "Empty application" from XCode5.1

---
+++ itrackmygps/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  itrackmygps
+//
+//  Created by rupert on 20/03/2014.
+//  Copyright (c) 2014 2RMobile Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ itrackmygps/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  itrackmygps
+//
+//  Created by rupert on 20/03/2014.
+//  Copyright (c) 2014 2RMobile Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
