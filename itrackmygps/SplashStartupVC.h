26908ce0da350d3b2121be6d11134f2ffa23f861
rupert <rupert@2rmobile.com>
2014-03-22 13:39:17 +1100

    Add SplashStartupVC

---
+++ itrackmygps/SplashStartupVC.h
@@ -0,0 +1,13 @@
+//
+//  SplashStartupVC.h
+//  itrackmygps
+//
+//  Created by rupert on 22/03/2014.
+//  Copyright (c) 2014 2RMobile Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashStartupVC : UIViewController
+
+@end
---
+++ itrackmygps/SplashStartupVC.m
@@ -0,0 +1,38 @@
+//
+//  SplashStartupVC.m
+//  itrackmygps
+//
+//  Created by rupert on 22/03/2014.
+//  Copyright (c) 2014 2RMobile Pty Ltd. All rights reserved.
+//
+
+#import "SplashStartupVC.h"
+
+@interface SplashStartupVC ()
+
+@end
+
+@implementation SplashStartupVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
